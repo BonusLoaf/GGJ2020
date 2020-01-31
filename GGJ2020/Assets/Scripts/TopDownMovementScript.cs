1635931df3aa2f83e280f2ba025439891172e8b0
sidskater <31853465+sidskater@users.noreply.github.com>
2020-01-31 18:48:20 +0000

    Added Unity project

---
+++ GGJ2020/Assets/Scripts/TopDownMovementScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownMovementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D)) { // Move diagonaly up right
+
+        } else if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S)) {  // Move diagonaly down right
+
+        } else if(Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A)) { // Move diagonaly down left
+
+        } else if(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W)) { // Move diagonaly up left
+
+        } else if(Input.GetKey(KeyCode.W)) { // Move Up
+
+        } else if (Input.GetKey(KeyCode.D)) { // Move right
+
+        } else if (Input.GetKey(KeyCode.S)) { // Move down
+
+        } else if (Input.GetKey(KeyCode.A)) { // Move left
+
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rigibody.velocity = new Vector2(x, y) * 10;
+
+    }
+}
