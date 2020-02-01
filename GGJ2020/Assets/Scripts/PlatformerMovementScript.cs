68e4a02c57429f32a39686f67faf023e514ce3ae
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2020-02-01 18:59:30 +0000

    Added ability to dash
    Will only work if the players physics material is set to "bouncyBoi" and dashEnabled is set to true

--- GGJ2020/Assets/Scripts/PlatformerMovementScript.cs
+++ GGJ2020/Assets/Scripts/PlatformerMovementScript.cs
@@ -6,16 +6,29 @@ public class PlatformerMovementScript : MonoBehaviour
 {
 
     public float speed = 10;
+    public float dashVerticalSpeed;
+    public float dashSpeed = 20f;
 
     public bool jumpEnabled;
+    public bool dashEnabled;
+
+
+    public bool canWalk;
+    private bool hasDashed;
 
     public GameObject[] raycastOrigin;
     public float raycastLength = 0.01f;
+
+    private int bounces;
 
     private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        bounces = 0;
+        canWalk = true;
+        hasDashed = false;
+
         if(raycastOrigin == null)
         {
             Debug.LogError("Please reference the raycast origin game object in the Platformer movement script");
@@ -27,14 +40,36 @@ public class PlatformerMovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
 
-        rb.velocity = new Vector2(x * speed, rb.velocity.y);
-
+        if(canWalk)
+        {
+            float x = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(x * speed, rb.velocity.y);
+        }
+        
 
         if(Input.GetKeyDown(KeyCode.Space) && jumpEnabled)
         {
-            AttemptJump();
+
+            if(dashEnabled && !hasDashed)
+            {
+                if(!canJump())
+                {
+                    // DASH
+                    print("Dash");
+                    dash();
+                    
+                } else
+                {
+                    AttemptJump();
+                }
+                 
+
+            } else
+            {
+                AttemptJump();
+            }
+            
             
         }
     }
@@ -46,11 +81,11 @@ public class PlatformerMovementScript : MonoBehaviour
 
     void AttemptJump()
     {
-        print(canJump());
+        //print(canJump());
         if(canJump())
         {
+            hasDashed = false;
             rb.velocity = new Vector2(0, 10);
-
         }
         
     }
@@ -76,5 +111,40 @@ public class PlatformerMovementScript : MonoBehaviour
 
 
         return false;
+    }
+
+    private void dash()
+    {
+        canWalk = false;
+        
+
+        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f)
+        {
+            //print("Should dash + " + Input.GetAxisRaw("Horizontal"));
+
+            rb.AddForce(Vector3.right * dashSpeed * Input.GetAxisRaw("Horizontal") * Time.deltaTime, ForceMode2D.Impulse);
+            rb.AddForce(Vector3.up * dashVerticalSpeed * Time.deltaTime, ForceMode2D.Impulse);
+            //Vector3 x = rb.velocity;
+            //x += transform.right * Input.GetAxisRaw("Horizontal") * dashSpeed * Time.deltaTime;
+            //rb.velocity = x;
+
+        } else if(Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+        {
+            rb.AddForce(Vector3.up * dashSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical"), ForceMode2D.Impulse);
+        }
+
+
+        bounces = 0;
+        hasDashed = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bounces++;
+
+        if(bounces >= 4)
+        {
+            canWalk = true;
+        }
     }
 }
