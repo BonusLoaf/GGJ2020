cbf2aaae45a9d433c5cba9f8f515e87929c33c43
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2020-02-02 00:27:38 +0000

    Health system backend
    Lives are stored and once all have been lost, the player will die

---
+++ GGJ2020/Assets/HealthSystem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    public GameController gc;
+    public int lives;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lives = 3;
+        gc = GetComponent<GameController>();
+    }
+
+    public void SetLives(int newLives)
+    {
+        lives = newLives;
+    }
+
+    public void loseLife()
+    {
+        lives--;
+        updateHealthSystem();
+    }
+
+    public void updateHealthSystem()
+    {
+        updateUI();
+        if(lives <= 0)
+        {
+            gc.SendMessage("playerDied");
+        }
+    }
+
+
+    public void updateUI()
+    {
+        print(lives);
+    }
+
+    // TEMP PLEASE DELETE UPDATE FUNCTION IF FOUND
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            loseLife();
+        }
+    }
+
+
+}
