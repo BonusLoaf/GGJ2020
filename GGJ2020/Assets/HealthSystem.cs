4270b822f24e9d2555a6be6d34f4216f8c69d2d9
wmastersPlym <43572947+wmastersPlym@users.noreply.github.com>
2020-02-02 11:17:01 +0000

    Finished UI for healthSystem

--- GGJ2020/Assets/HealthSystem.cs
+++ GGJ2020/Assets/HealthSystem.cs
@@ -6,12 +6,18 @@ public class HealthSystem : MonoBehaviour
 {
     public GameController gc;
     public int lives;
+    public HealthSystemUI UI;
 
     // Start is called before the first frame update
     void Start()
     {
         lives = 3;
         gc = GetComponent<GameController>();
+
+        if(!UI)
+        {
+            Debug.Log("Warnig: UI Not found on the health system");    
+        }
     }
 
     public void SetLives(int newLives)
@@ -37,7 +43,21 @@ public class HealthSystem : MonoBehaviour
 
     public void updateUI()
     {
-        print(lives);
+        switch(lives)
+        {
+            case 3:
+                UI.setFullHealth();
+                break;
+            case 2:
+                UI.setHalfHealth();
+                break;
+            case 1:
+                UI.setLittleHealth();
+                break;
+            case 0:
+                UI.setNoHealth();
+                break;
+        }
     }
 
     // TEMP PLEASE DELETE UPDATE FUNCTION IF FOUND
---
+++ GGJ2020/Assets/HealthSystemUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSystemUI : MonoBehaviour
+{
+
+    public Sprite fullHealth;
+    public Sprite halfHealth;
+    public Sprite littleHealth;
+    public Sprite dead;
+
+    private Image img;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = gameObject.GetComponent<Image>();
+        img.sprite = fullHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setFullHealth()
+    {
+        img.sprite = fullHealth;
+    }
+
+    public void setHalfHealth()
+    {
+        img.sprite = halfHealth;
+    }
+
+    public void setLittleHealth()
+    {
+        img.sprite = littleHealth;
+    }
+
+    public void setNoHealth()
+    {
+        img.sprite = dead;
+    }
+}
